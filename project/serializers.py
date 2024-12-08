8e60a7008fc679f2b15acc52d4dd42513bbc4244
Azizbek Sharifov <91140810+AzizbekSharifov@users.noreply.github.com>
2024-12-09 00:44:13 +0900

    :rocket: Add Images model and update serializers, views, and templates for image handling (#3)
    Co-authored-by: Javokhir Yuldoshev <aliyuldashev880@gmail.com>

--- project/serializers.py
+++ project/serializers.py
@@ -3,3 +3,4 @@ from rest_framework import serializers
 class ImageUploadSerializer(serializers.Serializer):
     file = serializers.FileField()
     camera_index = serializers.IntegerField()
+    text = serializers.CharField()
--- project/views.py
+++ project/views.py
@@ -1,18 +1,13 @@
-import time
-
-from django.shortcuts import render,HttpResponse,redirect
+from django.shortcuts import render,redirect
 import requests
-import numpy as np
-from .models import Stock
+
+from .models import Images
 from .serializers import ImageUploadSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
-import os
+
 from django.views.decorators.csrf import csrf_exempt
-
-from django.http import JsonResponse
-
 search_object = None  
 
 def home_page(request):
@@ -22,63 +17,34 @@ def home_page(request):
 
 def initial_page(request):
     if request.method =="POST":
+        objecte = request.POST.get("object")
+        requests.post("http://192.168.219.103:5000/search_object",data={"item_name":objecte})
         return redirect("output")
     return render(request,template_name="index.html",)
-
-
-
-
-
-
-
-
-
-
-def investment_summary_view(request):
-    # Define object symbols
-    global search_object
-    stock_symbols = request.POST.getlist("ticker")
-    time.sleep(5)
-    if "smartphone" in stock_symbols :
-        search_object = "smartphone"
-        context = {
-            "image":"static/images/image_phone.jpg"
-        }
-    else:
-        search_object = "laptop"
-        context = {
-            "image": "static/images/laptop_laptop.jpg"
-        }
-    
-    return render(request, 'output.html', context)
 
 
 @csrf_exempt
 @api_view(['POST'])
 def upload_image(request):
-    # Initialize the serializer with the incoming data
     serializer = ImageUploadSerializer(data=request.data)
 
     if serializer.is_valid():
         # Get the camera index and file from the request
         camera_index = serializer.validated_data['camera_index']
-        uploaded_file = serializer.validated_data['file']
+        uploaded_file = serializer.validated_data['files']
+        Images.objects.create(image=uploaded_file,indexs=camera_index).save()
+        
 
-        # Save the uploaded file (you can change the directory as needed)
-        file_path = os.path.join('/Users/sharifovazizbek/IOT/Iot/found_images', f"Camera{camera_index}_{uploaded_file.name}")
-        with open(file_path, 'wb') as f:
-            for chunk in uploaded_file.chunks():
-                f.write(chunk)
-
-        return Response({"message": f"File received from Camera {camera_index} and saved to {file_path}."}, status=status.HTTP_201_CREATED)
+        return Response("",status=status.HTTP_201_CREATED)
     else:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-def get_search_object(request):
-    global search_object
-    if search_object:
-        header = {"search_object": search_object}
-        return JsonResponse(header)
-    return JsonResponse({"search_object": None})
+def render_view(request):
+    if request.method == "POST":
+        Images.objects.all().delete()
+        return redirect("index")
+    
+    images = Images.objects.all()
+    return render(request,template_name="output.html",context={"images":images})
 
