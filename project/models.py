a5e87304f7e7a95368682c4fdb8bc1d5c8860ff8
Javokhir Yuldoshev <aliyuldashev880@gmail.com>
2024-12-09 00:35:39 +0900

    :rocket: Add Images model and update serializers, views, and templates for image handling

--- project/models.py
+++ project/models.py
@@ -5,3 +5,8 @@ class Stock(models.Model):
     image = models.ImageField(upload_to="images",default=None)
     def __str__(self):
         return self.ticker
+class Images(models.Model):
+    image = models.ImageField(upload_to="images",default=None)
+    indexs = models.IntegerField(default=0)
+    def __str__(self):
+        return self.image
